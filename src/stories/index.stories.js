c444cf1a274151442daf7f0d50cac3eb3187738e
Dan Steren <daniel.d.steren@gmail.com>
2018-10-23 00:42:03 -0600

    Add PreviewCard stories

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
 
 import MyButton from '../components/MyButton.vue'
+import PreviewCard from '../components/PreviewCard.vue'
 
 storiesOf('Button', module)
   .add('with text', () => ({
@@ -13,5 +14,44 @@ storiesOf('Button', module)
   .add('with some emoji', () => ({
     components: { MyButton },
     template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+    methods: { action: action('clicked') }
+  }))
+
+storiesOf('PreviewCard', module)
+  .add('with primary and secondary text', () => ({
+    components: { PreviewCard },
+    template: '<PreviewCard primary-text="New Document" secondary-text="Modified 10/22/18"/>',
+    methods: { action: action('clicked') }
+  }))
+  .add('with menu', () => ({
+    components: { PreviewCard },
+    template: `
+      <PreviewCard primary-text="New Document" secondary-text="Modified 10/22/18">
+        <template slot="menu">
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-action><v-icon>file_copy</v-icon></v-list-tile-action>
+              <v-list-tile-title>Create a copy</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-action><v-icon>edit</v-icon></v-list-tile-action>
+              <v-list-tile-title>Rename</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-action><v-icon>delete</v-icon></v-list-tile-action>
+              <v-list-tile-title>Remove</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-action><v-icon>open_in_new</v-icon></v-list-tile-action>
+              <v-list-tile-title>Edit in New Tab</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile>
+              <v-list-tile-action><v-icon>person</v-icon></v-list-tile-action>
+              <v-list-tile-title>Used By</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </template>
+      <PreviewCard>
+    `,
     methods: { action: action('clicked') }
   }))
